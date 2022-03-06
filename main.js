96b8f72631c8417396724353e5343566efe07669
Jesus Murguia <jesusmurguiafac@gmail.com>
2022-03-06 12:37:12 -0700

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector("[roll-dice]");
+const id = document.querySelector("[data-id]");
+const content = document.querySelector("[data-content");
+
+btn.addEventListener("click", (e) => {
+	newAdvice();
+});
+
+const newAdvice = () => {
+	btn.classList.add("animate");
+	fetch("https://api.adviceslip.com/advice")
+		.then((res) => res.json())
+		.then((data) => {
+			btn.classList.remove("animate");
+			id.innerHTML = `ADVICE ${data.slip.id}`;
+			content.innerHTML = `"${data.slip.advice}"`;
+		});
+};
